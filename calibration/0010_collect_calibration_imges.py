0cabce614ec5444b93a2a2c9821da4fa06d97ca4
Evert Pocoma <evertismael@gmail.com>
2025-02-27 16:09:42 +0100

    refactoring camera calibration

---
+++ calibration/0010_collect_calibration_imges.py
@@ -0,0 +1,74 @@
+import cv2
+import os
+import time, argparse
+from datetime import datetime
+from libs.utils import detect_chess_board_points
+
+
+def collect_camera_calibration_images(args):
+    '''
+    It opens a camera view and tries to detect the points of a chessboard.
+    When pressed 'c' it saves two images 
+        1. *_img.png is the image to be used later on.
+        2. *_chess.png is the same as *_img.png but with the chessboard points drawn over the image.
+    Notice that the saving is refused if the chessboard is not detected.
+    '''
+
+    # init camera with single image buffer
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_BUFFERSIZE,1)
+    a=cam.get(cv2.CAP_PROP_BUFFERSIZE) 
+    if not os.path.exists(args.out_dir):
+        os.makedirs(args.out_dir)
+
+    # collect images:
+    idx = 0
+    while True:
+        ret, frame = cam.read()
+        frame_to_save = frame.copy()
+        
+        # try to detect the chessboard
+        ptrn_size = ((6,4))
+        ret, P_chs, P_pxl = detect_chess_board_points(frame, ptrn_size)
+
+        # if detected draw:
+        if ret==True:
+            cv2.drawChessboardCorners(frame,ptrn_size,P_pxl,ret)
+            for p_idx, p in enumerate(P_pxl):
+                p = [int(a) for a in p]
+                cv2.putText(frame,str(p_idx),p,cv2.FONT_HERSHEY_COMPLEX,.4,(0,0,0),1,1)
+
+        cv2.imshow('camera',frame)
+
+
+        pressed_key = cv2.waitKey(1)
+        if pressed_key==ord('q'):
+            break
+        elif pressed_key==ord('c'):
+            if ret==True:
+                # check if directory exists
+                if not os.path.exists(args.out_dir):
+                    os.makedirs(args.out_dir)
+                print('capturing image')
+                filename = args.out_dir+'/'+str(idx)+'_img.png'
+                cv2.imwrite(filename, frame_to_save)
+
+                filename = args.out_dir+'/'+str(idx)+'_chess.png'
+                cv2.imwrite(filename, frame)
+                
+                print('-------------------------------------------------')
+                print(f'image {filename} saved')
+                print('-------------------------------------------------')
+                time.sleep(1)
+                idx+=1
+            else:
+                print(f'Chess not in image - NOT SAVED')
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(prog='CamImgCollect', description='Collection of images for camera calibration. Intrinsic Params')
+    parser.add_argument('-o', '--out_dir', type=str, default='./cal_imgs/'+datetime.now().strftime('%m%d-%H%M%S'), help='OutputDirectory')
+    args = parser.parse_args()
+    collect_camera_calibration_images(args)
---
+++ calibration/0020_calibrate_camera.py
@@ -0,0 +1,70 @@
+import cv2, glob, argparse, os
+import numpy as np
+from datetime import datetime
+from libs.utils import find_chessboard_on_image_files
+
+
+
+
+def calibrate_camera(args):
+    
+    # get all images in directory:
+    chess_img_files = glob.glob(args.img_dir+'/*_img.png')
+
+
+    # Step1: Collect all sequence of points for each chessboard, 
+    #        as well as the ones in chess coords.
+    print(chess_img_files)
+    ret_list, P_chs_list, P_pxl_list,img_size = find_chessboard_on_image_files(chess_img_files, args.pattern_size,args.scale)
+    
+    # select the pairs that are valid:
+    P_chs_list = [P_chs for P_chs,rl in zip(P_chs_list,ret_list) if rl==True]
+    P_pxl_list = [P_pxl for P_pxl,rl in zip(P_pxl_list,ret_list) if rl==True]
+
+    if not P_chs_list or not P_pxl_list:
+        print('Chessboard was not found in none of the images')
+        return
+    # Step2: Compute the calibration, and return the matrices: (img_size(is reverted))
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(P_chs_list,P_pxl_list,img_size[::-1],None,None)
+
+
+    # -------------------------------------------------------------
+    # Step3: show location of points on all image:
+    for i in range(len(chess_img_files)):
+        print(i)
+        frame = cv2.imread(chess_img_files[i])
+        frame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+        #frame = cv2.resize(frame,(640,480))
+        cv2.drawChessboardCorners(frame,args.pattern_size,P_pxl_list[i],True)
+        for p_idx, p in enumerate(P_pxl_list[i]):
+            p = [int(a) for a in p]
+            cv2.putText(frame,str(p_idx),p,cv2.FONT_HERSHEY_COMPLEX,.4,(0,0,0),1,1)
+        cv2.imshow('example',frame)
+        cv2.waitKey(0)
+
+    # saving files:
+    if args.scale:
+        out_file_pref = '/scaled'
+    else:
+        out_file_pref = '/'
+
+    save_flag = input(f'save output matrices in {args.out_dir} [yes/no]:')
+    if 'yes' in save_flag:
+        if not os.path.exists(args.out_dir):
+            os.makedirs(args.out_dir)
+        np.save(args.out_dir + out_file_pref + '_mtx.npy', mtx)
+        np.save(args.out_dir + out_file_pref + '_dist.npy', dist)
+        np.save(args.out_dir + out_file_pref + '_imgsize.npy', img_size)
+        print(f'saved in {args.out_dir + out_file_pref}')
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(prog='CamCalib', description='Based on images of a given directory, it estimates the camera intrinsic parameters')
+    parser.add_argument('-i', '--img_dir', type=str, default='./cal_imgs/000_default/', help='directory of calibration images')
+    parser.add_argument('-o', '--out_dir', type=str, default='./out/'+datetime.now().strftime('%m%d-%H%M%S'), help='output directory to save intrinsic params')
+    parser.add_argument('-s', '--scale', type=bool, default=False, help='in case the image is too big, it resize the image to (640,480)')
+    parser.add_argument('-p','--pattern_size', type=str, default="[6,4]", help='pattern size, default [6,4]')
+    args = parser.parse_args()
+    args.pattern_size = list(map(int, args.pattern_size.strip('[]').split(',')))
+
+    calibrate_camera(args)
+
